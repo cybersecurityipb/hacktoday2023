e72ca0254b40aaee78da2475aa7bfe4697210581
Anro128 <129676359+Anro128@users.noreply.github.com>
2023-08-14 13:28:30 +0700

    Add files via upload

---
+++ qual/misc/DCHEZKIBOXS/solver.py
@@ -0,0 +1,44 @@
+from pwn import *
+
+def get_password(s):
+    pl="DCHEZKIBOXS"
+    b={}
+    for i in range(65,91):
+        b[chr(i)]=0
+    for i in range(0,len(pl)):
+        b[pl[i]]=1
+    n=len(s)
+    l=0
+    r=l
+    v=[]
+    mx=0
+    password=""
+    while(l<n and r<n):
+        if(b[s[l]]==1):
+            if(r+1>=n):
+                break
+            r+=1
+            while(b[s[r]]==1 and r<n):
+                r+=1
+                if(r==n):
+                    break
+            tmp=s[l:r]
+            sz=len(tmp)
+            if(sz>mx):
+                mx=sz
+                password=tmp
+            l=r
+        else:
+            r=l
+        l+=1
+
+    return password
+
+
+p=remote('localhost',18001)
+t=p.recvline(0)
+s=t.decode()
+
+pw=get_password(s)
+p.sendlineafter("password: ", pw)
+p.interactive()
