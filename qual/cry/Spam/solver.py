e61e7800a57a5ac2264cd06dc56b1d48e3de536f
BIMASKUY <113542041+BIMASKUY@users.noreply.github.com>
2023-08-26 01:20:43 +0700

    Applied pwntools on solver.py

--- qual/cry/Spam/solver.py
+++ qual/cry/Spam/solver.py
@@ -1,34 +1,36 @@
+#!/usr/bin/env python3
+
 from Crypto.Util.number import bytes_to_long, long_to_bytes, inverse
 from sympy import primefactors
 from itertools import permutations
+from pwn import *
 
-with open('test.txt','r') as file:
-	file = file.read().splitlines()
+r = remote('localhost', 18000, level = 'debug')
 
-	e = 65537
-	maju = 0
-	full_password = []
-	
-	for indeks in range(0,len(file),2):
-		if indeks//2 == 9:
-			maju = 1
-		if indeks//2 == 99:
-			maju = 2 
+e = 65537
+full_password = []
 
-		n = int(file[indeks][5+maju:])
-		c = int(file[indeks+1][5+maju:])
+while True:
+	cek = r.recvuntil(b'= ')
+	if cek == b'\nInput Full Password = ':
+		break
+	n = int(r.recvline(0))
+	r.recvuntil(b'= ')
+	c = int(r.recvline(0))
 
-		factor = primefactors(n)
+	factor = primefactors(n)
+	p = factor[0]
+	q = factor[1]
+	phi = (p-1)*(q-1)
+	d = inverse(e,phi)
+	m = pow(c,d,n)
+	message = long_to_bytes(m)
 
-		p = factor[0]
-		q = factor[1]
-		phi = (p-1)*(q-1)
-		d = inverse(e,phi)
-		m = pow(c,d,n)
-		message = long_to_bytes(m).decode()
-		if 'happy_birthday' not in message:
-			full_password.append(message)
+	if b'happy_birthday' not in message:
+		full_password.append(message)
 
-	for kata in permutations(full_password):
-		if kata[0].endswith('_') and kata[2].startswith('_'):
-			print(''.join(kata))
+for kata in permutations(full_password):
+	if kata[0].endswith(b'_') and kata[2].startswith(b'_'):
+		password = b''.join(kata)
+		r.sendline(password)
+		r.recvall()
