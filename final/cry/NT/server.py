9b16e27495eafd9915b709ab36f9d002a598b76d
Muhammad Nafiz <101576842+MNafiz@users.noreply.github.com>
2023-09-20 07:24:58 +0700

    Update server.py

--- final/cry/NT/server.py
+++ final/cry/NT/server.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 
 from Crypto.Util.number import *
-import os, random, time
+import os, random, time, sys
+
+class Unbuffered(object):
+  def __init__(self, stream):
+    self.stream = stream
+  def write(self, data):
+    self.stream.write(data)
+    self.stream.flush()
+  def writelines(self, datas):
+    self.stream.writelines(datas)
+    self.stream.flush()
+  def __getattr__(self, attr):
+    return getattr(self.stream, attr)
+
+sys.stdout = Unbuffered(sys.stdout)
 
 with open("flag.txt","r") as f:
     FLAG = f.read().rstrip()
